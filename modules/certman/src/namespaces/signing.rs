caffa459c7ee5f9ef758c66aa891b17ec4fcbd64
annapravda <anyapravdaa@gmail.com>
2024-07-23 23:15:08 +0200

    Added generation certificate for signing

--- modules/certman/src/namespaces/signing.rs
+++ modules/certman/src/namespaces/signing.rs
@@ -3,7 +3,7 @@ use colored::Colorize;
 use libmilkyway::cli::arguments::parse_arguments;
 use libmilkyway::cli::router::CommandNamespace;
 use libmilkyway::cli::table::Table;
-use libmilkyway::pki::certificate::{Certificate, FLAG_CLIENT_CERT, FLAG_SIGN_CERTS};
+use libmilkyway::pki::certificate::{Certificate, FLAG_CLIENT_CERT, FLAG_NO_READ, FLAG_NO_WRITE, FLAG_SERVER_CERT, FLAG_SIGN_CERTS, FLAG_SIGN_MESSAGES, FLAG_USER_CERT};
 use libmilkyway::pki::hash::HashType;
 use libmilkyway::pki::impls::certificates::falcon1024::Falcon1024Certificate;
 use libmilkyway::pki::impls::keys::falcon1024::generate_falcon1024_keypair;
@@ -77,10 +77,124 @@ impl SigningNamespace {
             return Ok(certificate);
         }
     }
+    fn parse_flags(value: String) -> Option<u128> {
+        let flags = value.split(",");
+        let mut result = 0;
+        for flag in flags{
+            if flag == "no-read"{
+                result = result | FLAG_NO_READ;
+                continue;
+            }
+            if flag == "no-write" {
+                result = result | FLAG_NO_WRITE;
+                continue;
+            }
+            if flag == "sign-messages" {
+                result = result | FLAG_SIGN_MESSAGES;
+                continue;
+            }
+            if flag == "sign-certs" {
+                result = result | FLAG_SIGN_CERTS;
+                continue;
+            }
+            if flag == "client-cert" {
+                result = result | FLAG_CLIENT_CERT;
+                continue;
+            }
+            if flag == "server-cert" {
+                result = result | FLAG_SERVER_CERT;
+                continue;
+            }
+            if flag == "user-cert" {
+                result = result | FLAG_USER_CERT;
+                continue;
+            }
+            return None;
+        }
+        return Some(result);
+    }
 
 
+    // Arguments of comma.nd(those ones in argmap)
+    // * serial -- a serial number for new certificate
+    // * parent -- a serial number of parent certificate
+    // * name -- a name of certificate
+    // * flags -- flags list, optional(use parse_flags), if not provided default 0
     pub fn generate(&mut self, arguments: Vec<String>){
-        // 1) Get certificate and sign in separate function
+        let argmap = parse_arguments(arguments);
+        /* Check serial */
+        if !argmap.contains_key("serial"){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'serial' is required");
+            return;
+        }
+        let serial = argmap.get("serial").unwrap();
+        if serial.is_none(){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'serial' must have a value");
+            return;
+        }
+        let serial = serial.clone().unwrap();
+        let serial = serial.parse::<u128>();
+        if serial.is_err(){
+            println!("{} {}", "error:".red().bold().underline(),
+                     "Argument serial must be a positive number");
+            return;
+        }
+        let serial = serial.unwrap();
+        if !argmap.contains_key("parent"){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'parent' is required");
+            return;
+        }
+        let parent = argmap.get("parent").unwrap();
+        if parent.is_none(){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'parent' must have a value");
+            return;
+        }
+        let parent = parent.clone().unwrap();
+        let parent = parent.parse::<u128>();
+        if parent.is_err(){
+            println!("{} {}", "error:".red().bold().underline(),
+                     "Argument 'parent' must be a positive number");
+            return;
+        }
+        let parent = parent.unwrap();
+        if !argmap.contains_key("name"){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'name' is required");
+            return;
+        }
+        let name = argmap.get("name").unwrap();
+        if name.is_none(){
+            println!("{} {}", "error:".red().bold().underline(), "Argument 'name' requires a value");
+            return;
+        }
+        let name = name.clone().unwrap();
+        let mut flags = 0;
+        if argmap.contains_key("flags"){
+            let flags_argument =  argmap.get("flags").unwrap();
+            if flags_argument.is_none(){
+                println!("{} {}", "error:".red().bold().underline(), "Argument 'flags' requires a value");
+                return;
+            }
+            let flags_result = Self::parse_flags(flags_argument.clone().unwrap());
+            if flags_result.is_none(){
+                println!("{} {}", "error:".red().bold().underline(), "Argument 'flags' is invalid");
+                return;
+            }
+            flags = flags_result.unwrap();
+        }
+        let mut binder = self.cert_binder.lock().unwrap();
+        let signed_certificate = self.generate_signed_certificate(&mut binder,
+                                                                  serial, parent, name, flags);
+        if signed_certificate.is_err(){
+            println!("{} {}", "error:".red().bold().underline(),signed_certificate.err().unwrap());
+            return;
+        }
+        let signed_certificate = signed_certificate.unwrap();
+        let result = binder.add_signing_certificate(signed_certificate);
+        if !result{
+            println!("{} {}", "error:".red().bold().underline(), "Can not add certificate to servise");
+            return;
+        }
+        binder.commit();
     }
 
     pub fn remove(&mut self, arguments: Vec<String>){
